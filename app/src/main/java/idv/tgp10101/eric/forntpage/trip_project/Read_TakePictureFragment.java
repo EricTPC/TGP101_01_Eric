d8401f5fb0215b2c6491c76aa211ea62d991cd98
Eric <a0980358859@gmail.com>
2022-08-21 01:45:01 +0800

    更新APP (新增檢視專案功能)

---
+++ app/src/main/java/idv/tgp10101/eric/forntpage/trip_project/Read_TakePictureFragment.java
@@ -0,0 +1,66 @@
+package idv.tgp10101.eric.forntpage.trip_project;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import idv.tgp10101.eric.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link Read_TakePictureFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class Read_TakePictureFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Read_TakePictureFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Read_TakePictureFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static Read_TakePictureFragment newInstance(String param1, String param2) {
+        Read_TakePictureFragment fragment = new Read_TakePictureFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_read__take_picture, container, false);
+    }
+}
